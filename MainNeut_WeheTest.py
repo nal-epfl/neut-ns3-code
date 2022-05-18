f3cd40d0951b6d80edee4ffa9c8900a3dfd8c9ef
zeinabshmeis <zeinab.shmeis@epfl.ch>
2022-05-18 16:25:39 +0200

    Add the wehe test cases

--- MainNeut_WeheTest.py
+++ MainNeut_WeheTest.py
@@ -200,7 +200,7 @@ def run_parallel_experiments(func, experiments):
 
 def run_parallel_experiments_safe(func, experiments):
     running_processes = []
-    nb_threads = cpu_count() / 2 - 5
+    nb_threads = 5#cpu_count() / 2 - 5
 
     for idx, params in enumerate(experiments):
         time.sleep(5)
@@ -218,42 +218,110 @@ if __name__ == '__main__':
 
     ############################################################### Localization Tests ##############################################################
     netflix, hulu, youtube, twitch, facebook, vimeo = 'Netflix_12122018', 'Hulu_04112019', 'Youtube_12122018', 'Twitch_04112019', 'FacebookVideo_04112019', 'Vimeo_12122018'
-    TEST_TYPE = 'Wehe_Test_Cases'
+    TEST_TYPE = 'Wehe_Test_Cases_2'
     background_dir = 'chicago_2010_back_traffic_10min_control_cbp_2links'
 
     link_rate, noncommon_link_rates = '10Gbps', '1Gbps,1Gbps,10Gbps'
     is_tcp, a_seed = 1, 13
 
-    #for each user I need the following information
+    #for each user I need the following information: (user, app, duration, rtt, throttling_rate)
     wehe_tests = [
-        ('feqHGJ240F_9', hulu, 14, 45, [('shared_common_policer', 1, 42, 0.03), ('shared_noncommon_policers', 3, 21, 0.03)]),
-        ('198iWiLIfr_128', netflix, 19, 40, [('shared_common_policer', 1, 40, 0.03), ('shared_noncommon_policers', 3, 20, 0.03)]),
-        ('3xyk5naej3_1', vimeo, 27, 45, [('shared_common_policer', 1, 40, 0.03), ('shared_noncommon_policers', 3, 20, 0.03)]),
-        ('89twsgj1pb_263', youtube, 31, 45, [('shared_common_policer', 1, 27, 0.03), ('shared_noncommon_policers', 3, 13.5, 0.03)]),
-        ('9lwmntt0pt_8', youtube, 22, 110, [('shared_common_policer', 1, 28, 0.03), ('shared_noncommon_policers', 3, 14, 0.03)]),
-        ('2b98k2i1qy_1', youtube, 45, 109, [('shared_common_policer', 1, 25, 0.03), ('shared_noncommon_policers', 3, 12.5, 0.03)]),
+        [
+            ('feqHGJ240F_9', hulu, 14, 45, 38),
+            ('198iWiLIfr_128', netflix, 19, 40, 40),
+            ('3xyk5naej3_1', vimeo, 27, 45, 40),
+            ('89twsgj1pb_263', youtube, 31, 45, 27),
+            ('9lwmntt0pt_8', youtube, 22, 110, 28),
+            ('2b98k2i1qy_1', youtube, 45, 109, 25),
 
-        ('smzkg3b9c3_12', hulu, 40, 52, [('shared_common_policer', 1, 30, 0.03), ('shared_noncommon_policers', 3, 15, 0.03)]),
-        ('y3emavi0lu_22', twitch, 45, 60, [('shared_common_policer', 1, 25, 0.03), ('shared_noncommon_policers', 12.5, 15, 0.03)]),
-        ('v8azm5f2qe_30', twitch, 17, 59, [('shared_common_policer', 1, 40, 0.03), ('shared_noncommon_policers', 12.5, 20, 0.03)]),
-        ('v8azm5f2qe_21', vimeo, 23, 41, [('shared_common_policer', 1, 40, 0.03), ('shared_noncommon_policers', 12.5, 20, 0.03)]),
-        ('v8azm5f2qe_31', youtube, 18, 61, [('shared_common_policer', 1, 45, 0.03), ('shared_noncommon_policers', 12.5, 22.5, 0.03)]),
-        ('y3emavi0lu_17', youtube, 14, 82, [('shared_common_policer', 1, 45, 0.03), ('shared_noncommon_policers', 12.5, 22.5, 0.03)]),
+            ('smzkg3b9c3_12', hulu, 40, 52, 30),
+            ('y3emavi0lu_22', twitch, 45, 60, 25),
+            ('v8azm5f2qe_30', twitch, 17, 59, 40),
+            ('v8azm5f2qe_21', vimeo, 23, 41, 40),
+        ],
+        [
+            ('v8azm5f2qe_31', youtube, 18, 61, 45),
+            ('y3emavi0lu_17', youtube, 14, 82, 45),
+            ('89twsgj1pb_260', youtube, 35, 51, 25),
+            ('v8azm5f2qe_32', youtube, 18, 59, 27),
+            ('y3emavi0lu_18', youtube, 45, 82, 26),
 
+            ('oxgg4e3e4j_5', facebook, 45, 19, 25),
+            ('oxgg4e3e4j_14', hulu, 45, 63, 25),
+            ('oxgg4e3e4j_15', netflix, 45, 29, 40),
+            ('X6Jbefujhj_86', netflix, 45, 117, 38),
+            ('xKMno64HM4_82', netflix, 19, 84, 42),
+        ],
+        [
+            ('oxgg4e3e4j_2', twitch, 45, 74, 35),
+            ('xKMno64HM4_80', twitch, 16, 39, 41),
+            ('5h72loiv58_2', youtube, 9, 65, 46),
+            ('X6Jbefujhj_83', youtube, 45, 150, 26),
+            ('xKMno64HM4_84', youtube, 15, 39, 36),
+
+            ('ijjflyw91d_3', youtube, 9, 39, 44),
+            ('neue1vldmc_1', youtube, 30, 60, 29),
+
+            ('yR4OlMyZ8Q_160', netflix, 45, 29, 35),
+            ('PAIiliDVCF_26', youtube, 45, 76, 27),
+            ('yR4OlMyZ8Q_153', youtube, 45, 29, 25),
+        ],
+        [
+            ('nqtiltg0z6_17', youtube, 40, 137, 26),
+            ('89twsgj1pb_255', youtube, 31, 50, 30),
+            ('9zkj054unk_24', youtube, 29, 41, 31),
+            ('4k0ct5yekk_42', youtube, 9, 39, 45),
+
+            ('thbrjjd72o_4', facebook, 30, 105, 31),
+            ('httosd1ip3_5', twitch, 45, 104, 20),
+            ('hyc7kr7x6q_1', youtube, 16, 23, 42),
+
+            ('ijdWV3YRwR_36', hulu, 41, 29, 28),
+            ('y9z2ih99ml_4', hulu, 45, 74, 25),
+            ('tadxntkmqi_2', netflix, 45, 49, 38),
+        ],
+        [
+            ('ijdWV3YRwR_39', twitch, 42, 29, 38),
+            ('ijdWV3YRwR_40', vimeo, 45, 29, 35),
+            ('xphVeDZef0_3', vimeo, 14, 37, 45),
+            ('ijdWV3YRwR_46', youtube, 44, 45, 40),
+            ('39hdicOl3j_8', youtube, 30, 22, 44),
+            ('8aY9jds9sG_3', youtube, 44, 30, 40),
+            ('89twsgj1pb_253', youtube, 37, 46, 42),
+            # ('', twitch, 45, 74, ),
+            # ('', twitch, 45, 74, ),
+            # ('', twitch, 45, 74, ),
+        ]
 
     ]
 
-    for a_seed in [3, 7]:
+    for wehe_test_batch in wehe_tests:
         exp_params = []
-        for user, wehe_app, rtt, duration, policers_configs in wehe_tests:
+        for user, wehe_app, duration, rtt, throttling_rate in wehe_test_batch:
             dp = (rtt-10)/2
-            for p_type, is_neutral, policing_rate, burst_length in policers_configs:
-                exp_params.append(ExperimentParameters(
-                    link_rate=link_rate, duration=duration, is_tcp=is_tcp, tcp_protocol='TcpCubic', seed=a_seed,
-                    app_type=5, app_name=wehe_app, background_dir=background_dir,
-                    exp_batch='{}/{}_{}Mbps_{}s_30p'.format(user, p_type, policing_rate, burst_length),
-                    noncommon_links_delays='{}ms,{}ms,5ms'.format(dp, dp), noncommon_links_rates=noncommon_link_rates,
-                    is_neutral=is_neutral, policing_rate=policing_rate, burst_length=burst_length
-                ))
+            p_type, is_neutral, policing_rate, burst_length = 'shared_common_policer', 1, throttling_rate, 0.03
+            exp_params.append(ExperimentParameters(
+                link_rate=link_rate, duration=duration, is_tcp=is_tcp, tcp_protocol='TcpCubic', seed=3,
+                app_type=5, app_name=wehe_app, background_dir=background_dir,
+                exp_batch='{}/{}_{}Mbps_{}s_30p'.format(user, p_type, policing_rate, burst_length),
+                noncommon_links_delays='{}ms,{}ms,5ms'.format(dp, dp), noncommon_links_rates=noncommon_link_rates,
+                is_neutral=is_neutral, policing_rate=policing_rate, burst_length=burst_length
+            ))
+        run_parallel_experiments(run_weheCS_experiment_with_params, exp_params)
 
-        run_parallel_experiments_safe(run_weheCS_experiment_with_params, exp_params)
+    # for user, wehe_app, duration, rtt, throttling_rate in wehe_tests:
+    #     dp = (rtt-10)/2
+    #     policers_configs = [
+    #         ('shared_common_policer', 1, throttling_rate, 0.03),
+    #         # ('shared_noncommon_policers', 3, throttling_rate/2, 0.03)
+    #     ]
+    #     for p_type, is_neutral, policing_rate, burst_length in policers_configs:
+    #         run_parallel_experiments(run_weheCS_experiment_with_params, [
+    #             ExperimentParameters(
+    #                 link_rate=link_rate, duration=duration, is_tcp=is_tcp, tcp_protocol='TcpCubic', seed=a_seed,
+    #                 app_type=5, app_name=wehe_app, background_dir=background_dir,
+    #                 exp_batch='{}/{}_{}Mbps_{}s_30p'.format(user, p_type, policing_rate, burst_length),
+    #                 noncommon_links_delays='{}ms,{}ms,5ms'.format(dp, dp), noncommon_links_rates=noncommon_link_rates,
+    #                 is_neutral=is_neutral, policing_rate=policing_rate, burst_length=burst_length
+    #             ) for a_seed in [3, 7]
+    #         ])
